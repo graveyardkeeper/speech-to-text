3e6a52dc7b58c64e8b3a04b4aba51ff432f70467
graveyardkeeper <naughty_dog@qq.com>
2023-06-27 23:46:30 +0800

    feat: use ffmpeg stream

--- main.go
+++ main.go
@@ -2,34 +2,40 @@ package main
 
 // [START speech_transcribe_streaming_mic]
 import (
-	"bytes"
 	"context"
-	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
+	"os/exec"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	"cloud.google.com/go/speech/apiv1/speechpb"
-	"github.com/gordonklaus/portaudio"
 )
-
-func float32ToByte(f []float32) []byte {
-	var buf bytes.Buffer
-	err := binary.Write(&buf, binary.BigEndian, f)
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	return buf.Bytes()
-}
 
 const sampleRate = 16000
 
 func main() {
 	ctx := context.Background()
 
-	portaudio.Initialize()
-	defer portaudio.Terminate()
+	cmdName := "ffmpeg"
+	args := []string{
+		"-f",
+		"avfoundation",
+		"-i",
+		":0",
+		"-ar",
+		fmt.Sprintf("%d", sampleRate),
+		"-vn",
+		"-f",
+		"flac",
+		"pipe:", // to pipe
+	}
+	cmd := exec.Command(cmdName, args...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalf("cmd.StdoutPipe(): %v", err)
+		return
+	}
 
 	client, err := speech.NewClient(ctx)
 	if err != nil {
@@ -44,9 +50,10 @@ func main() {
 		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
 			StreamingConfig: &speechpb.StreamingRecognitionConfig{
 				Config: &speechpb.RecognitionConfig{
-					Encoding:        speechpb.RecognitionConfig_LINEAR16,
+					Encoding:        speechpb.RecognitionConfig_FLAC,
 					SampleRateHertz: sampleRate,
-					LanguageCode:    "en-US",
+					//LanguageCode:    "en-US",
+					LanguageCode: "zh-CN",
 				},
 			},
 		},
@@ -54,34 +61,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	buf := make([]int32, 64)
-	microphone, err := portaudio.OpenDefaultStream(1, 0, sampleRate, len(buf), buf)
-
-	microphone.Start()
-	defer microphone.Close()
-
-	println("##")
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("cmd.Start(): %v", err)
+		return
+	}
 	go func() {
 		// Pipe stdin to the API.
+		buf := make([]byte, 1024)
 		for {
-			//n, err := os.Stdin.Read(buf)
-			if err := microphone.Read(); err != nil {
-				log.Fatalf("Read: %+v", err)
-				break
-			}
-			var b = &bytes.Buffer{}
-			err := binary.Write(b, binary.BigEndian, buf)
-			if err != nil {
-				panic(err)
-			}
-			if len(b.Bytes()) > 0 {
+			n, err := stdout.Read(buf)
+			if n > 0 {
 				if err := stream.Send(&speechpb.StreamingRecognizeRequest{
 					StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
-						AudioContent: b.Bytes(),
+						AudioContent: buf[:n],
 					},
 				}); err != nil {
 					log.Printf("Could not send audio: %v", err)
 				}
+			}
+			if err == io.EOF {
+				println("pipe EOF")
+				// Nothing else to pipe, close the stream.
+				if err := stream.CloseSend(); err != nil {
+					log.Fatalf("Could not close stream: %v", err)
+				}
+				return
+			}
+			if err != nil {
+				log.Printf("Could not read from stdin: %v", err)
+				continue
 			}
 		}
 	}()
